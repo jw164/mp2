69db67a560847d54e6889c3045ea334bf7c428f5
jw164 <jw164@illinois.edu>
2025-10-04 17:04:11 -0500

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import ListView from "./pages/ListView";
+import GalleryView from "./pages/GalleryView";
 import DetailView from "./pages/DetailView";
+import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
     <Router basename="/mp2">
+      <NavBar />
       <Routes>
         <Route path="/" element={<ListView />} />
+        <Route path="/gallery" element={<GalleryView />} />
         <Route path="/pokemon/:id" element={<DetailView />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
 }
+
