d05ad244248effe7cfd6214e7737e2acf6affdef
jw164 <jw164@illinois.edu>
2025-10-04 16:41:56 -0500

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import ListView from "./pages/ListView";
 import DetailView from "./pages/DetailView";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/mp2">
+    <Router basename="/mp2">
       <Routes>
         <Route path="/" element={<ListView />} />
         <Route path="/pokemon/:id" element={<DetailView />} />
+        {/* Fallback route: redirect any unknown path to the list */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
