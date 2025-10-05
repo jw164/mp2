d48eef28731ba3cf59812c1cdb156a4316c85490
jw164 <jw164@illinois.edu>
2025-10-05 12:50:24 -0500

    types.ts

--- src/types.ts
+++ src/types.ts
@@ -6,15 +6,17 @@ export type PokemonListItem = {
 export type Pokemon = {
   id: number;
   name: string;
-  base_experience: number;
   height: number;
   weight: number;
+  base_experience?: number | null;
   sprites: {
-    front_default: string | null;
+    front_default?: string | null;
     other?: {
-      ["official-artwork"]?: { front_default?: string | null };
+      ["official-artwork"]?: {
+        front_default?: string | null;
+      };
     };
   };
-  types?: { slot: number; type: { name: string; url: string } }[];
-  stats?: { base_stat: number; stat: { name: string } }[];
+  types: { slot: number; type: { name: string } }[];
+  stats: { base_stat: number; stat: { name: string } }[];
 };
